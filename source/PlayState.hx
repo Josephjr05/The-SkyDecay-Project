b93da2a880215522ce2322cff4c6995bbdff4adb
Joseph <josephjr1506@gmail.com>
2022-10-07 17:31:49 -0400

    Messing around with shit

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -82,14 +82,14 @@ class PlayState extends MusicBeatState
 	public static var STRUM_X_MIDDLESCROLL = -278;
 
 	public static var ratingStuff:Array<Dynamic> = [
-		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
-		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
-		['Good', 0.8], //From 70% to 79%
-		['Great', 0.9], //From 80% to 89%
+		['Do Better', 0.2], //From 0% to 19%
+		['Try Harder', 0.4], //From 20% to 39%
+		['Badddd', 0.5], //From 40% to 49%
+		['Hmm', 0.6], //From 50% to 59%
+		['Mehhh', 0.69], //From 60% to 68%
+		['OkNice', 0.7], //69%
+		['Good Job', 0.8], //From 70% to 79%
+		['Doing Great', 0.9], //From 80% to 89%
 		['Sick!', 1], //From 90% to 99%
 		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];

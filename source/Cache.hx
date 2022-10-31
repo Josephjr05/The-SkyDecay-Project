ecf2ac926be3746fb4e0d1c111afb7306f6b85a0
Joseph <josephjr1506@gmail.com>
2022-10-31 17:41:01 -0400

    Cache

--- source/Cache.hx
+++ source/Cache.hx
@@ -47,7 +47,7 @@ class Cache extends MusicBeatState
 		bitmapData = new Map<String,FlxGraphic>();
 		bitmapData2 = new Map<String,FlxGraphic>();
 
-		var menuBG:FlxSprite = new FlxSprite().loadGraphic(Paths.image('loadingScreens/loadingscreen-' + FlxG.random.int(1, 4)));
+		var menuBG:FlxSprite = new FlxSprite().loadGraphic(Paths.image('loadingScreens/loadingscreen-' + FlxG.random.int(1, 3)));
 menuBG.screenCenter();
 add(menuBG);
 

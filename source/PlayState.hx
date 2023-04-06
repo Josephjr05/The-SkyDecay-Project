c6bdd20a6be1f9be73494137a1f0661774f4d0c4
Joseph <josephjr1506@gmail.com>
2023-04-05 20:07:24 -0400

    Updated playstate and yea

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -330,6 +330,29 @@ class PlayState extends MusicBeatState
 	// stores the last combo score objects in an array
 	public static var lastScore:Array<FlxSprite> = [];
 
+	//camellia concert crowds
+	var frontCrowd:FlxSprite;
+	var backCrowd:FlxSprite;
+	var zooming:Bool = false;
+	//other concert shit
+	var sky:FlxSprite;
+	var stage:FlxSprite;
+	//the speakers
+	var speakerLeft:FlxSprite;
+	var speakerRight:FlxSprite;
+	var speakerLeft2:FlxSprite;
+	var speakerRight2:FlxSprite;
+	//Light stuff
+	var light:FlxSprite;
+	var citycycle:Int = 1;
+	var lightcycle:String = ClientPrefs.lightcycle;
+
+	//Star Stuff
+	var star:FlxSprite;
+	var spin:Bool = false;
+	var kuraCool:Bool = false;
+	var kurama:Bool = false;
+
 	//The Song Boxes
 	var box:FlxSprite;
 	var songNameText:FlxText;
@@ -538,6 +561,53 @@ class PlayState extends MusicBeatState
 
 		switch (curStage)
 		{
+			case 'concert':
+				sky = new FlxSprite(60, 40).loadGraphic(Paths.image('camellia/Week2/sky'));
+				sky.scale.set(1.75, 1.75);
+				add(sky);
+
+				light = new FlxSprite(60, 40).loadGraphic(Paths.image('camellia/Week2/light'));
+				light.scale.set(1.75, 1.75);
+				add(light);
+
+				backCrowd = new FlxSprite(60, 590).loadGraphic(Paths.image('camellia/Week2/backcrowd'));
+				backCrowd.scale.set(1.75, 1.75);
+				add(backCrowd);
+				
+				star = new FlxSprite(900, 2270).loadGraphic(Paths.image('camellia/Week2/ninestars'));
+				star.scale.set(2.5, 2.5);
+				add(star);
+
+				stage = new FlxSprite(-950, -505).loadGraphic(Paths.image('camellia/Week2/stage'));
+				stage.scale.set(0.85, 0.85);
+				add(stage);
+
+				speakerLeft = new FlxSprite(-150, 150);
+				speakerLeft.frames = Paths.getSparrowAtlas('camellia/Week2/speaker_left');
+				speakerLeft.animation.addByPrefix('bop', 'speaker', 24, false);
+				speakerLeft.scale.set(0.75, 0.75);
+				add(speakerLeft);
+
+				speakerRight = new FlxSprite(1530, 150);
+				speakerRight.frames = Paths.getSparrowAtlas('camellia/Week2/speaker_left');
+				speakerRight.animation.addByPrefix('bop', 'speaker', 24, false);
+				speakerRight.flipX = true;
+				speakerRight.scale.set(0.75, 0.75);
+				add(speakerRight);
+
+				speakerLeft2 = new FlxSprite(140, 250);
+				speakerLeft2.frames = Paths.getSparrowAtlas('camellia/Week2/speaker_left');
+				speakerLeft2.animation.addByPrefix('bop', 'speaker', 24, false);
+				speakerLeft2.scale.set(0.5, 0.5);
+				add(speakerLeft2);
+
+				speakerRight2 = new FlxSprite(1260, 250);
+				speakerRight2.frames = Paths.getSparrowAtlas('camellia/Week2/speaker_left');
+				speakerRight2.animation.addByPrefix('bop', 'speaker', 24, false);
+				speakerRight2.flipX = true;
+				speakerRight2.scale.set(0.5, 0.5);
+				add(speakerRight2);
+
 			case 'stage': //Week 1
 				var bg:BGSprite = new BGSprite('stageback', -600, -200, 0.9, 0.9);
 				add(bg);
@@ -597,6 +667,11 @@ class PlayState extends MusicBeatState
 		{
 			case 'camellia':
 				//you really thought, imagine
+			case 'concert':
+				frontCrowd = new FlxSprite(60, 1000).loadGraphic(Paths.image('camellia/Week2/frontcrowd'));
+				frontCrowd.alpha = 0;
+				frontCrowd.scale.set(1.75, 1.75);
+				add(frontCrowd);
 		}
 
 		box = new FlxSprite().makeGraphic(FlxG.width, 120, FlxColor.BLACK);
@@ -1578,7 +1653,7 @@ class PlayState extends MusicBeatState
 				cutsceneHandler.timer(3, function()
 				{
 					camFollow.x += 750;
-					camFollow.y += 100;
+					camFollow.y += 150;
 				});
 
 				// Beep!
@@ -2613,6 +2688,19 @@ class PlayState extends MusicBeatState
 			iconP1.swapOldIcon();
 		}*/
 
+		switch(curStage)
+		{
+			case 'concert':
+				camFollow.x = 1050;
+				camFollow.y = 390;
+				isCameraOnForcedPos = true;
+				if(kurama) {
+					boyfriend.color = 0xff474747;
+					dad.color = 0xff474747;
+					gf.color = 0xff474747;
+				}
+		}
+
 		callOnLuas('onUpdate', [elapsed]);
 
 		if(kmMode)
@@ -3066,15 +3154,61 @@ class PlayState extends MusicBeatState
 		badApple = !badApple;
 	}
 
+	function kcmMode(kuramaInt:Int)
+	{
+		trace(kuramaInt);
+		kuraCool = true;
+		if(kuramaInt == 1)
+		{
+			FlxTween.color(backCrowd, 1.3, FlxColor.WHITE, 0xff474747, {ease: FlxEase.linear});
+			FlxTween.color(stage, 1.3, FlxColor.WHITE, 0xff474747, {ease: FlxEase.linear});
+			FlxTween.color(speakerLeft, 1.3, FlxColor.WHITE, 0xff474747, {ease: FlxEase.linear});
+			FlxTween.color(speakerRight, 1.3, FlxColor.WHITE, 0xff474747, {ease: FlxEase.linear});
+			FlxTween.color(speakerLeft2, 1.3, FlxColor.WHITE, 0xff474747, {ease: FlxEase.linear});
+			FlxTween.color(speakerRight2, 1.3, FlxColor.WHITE, 0xff474747, {ease: FlxEase.linear});
+			FlxTween.color(frontCrowd, 1.3, FlxColor.WHITE, 0xff474747, {ease: FlxEase.linear});
+			FlxTween.color(boyfriend, 1.3, FlxColor.WHITE, 0xff474747, {ease: FlxEase.linear});
+			FlxTween.color(dad, 1.3, FlxColor.WHITE, 0xff474747, {ease: FlxEase.linear});
+			FlxTween.color(gf, 1.3, FlxColor.WHITE, 0xff474747, {ease: FlxEase.linear});
+			FlxTween.tween(star, {y: 190}, 2.3, {ease: FlxEase.backInOut, onComplete: function(twn:FlxTween)
+			{
+				spin = true;
+				kuraCool = false;
+				kurama = true;
+			}});
+		}
+		if(kuramaInt == 2)
+		{
+			kurama = false;
+			FlxTween.color(backCrowd, 1.3, 0xff474747, 0xFFFFFFFF, {ease: FlxEase.linear});
+			FlxTween.color(stage, 1.3, 0xff474747, 0xFFFFFFFF, {ease: FlxEase.linear});
+			FlxTween.color(speakerLeft, 1.3, 0xff474747, 0xFFFFFFFF, {ease: FlxEase.linear});
+			FlxTween.color(speakerRight, 1.3, 0xff474747, 0xFFFFFFFF, {ease: FlxEase.linear});
+			FlxTween.color(speakerLeft2, 1.3, 0xff474747, 0xFFFFFFFF, {ease: FlxEase.linear});
+			FlxTween.color(speakerRight2, 1.3, 0xff474747, 0xFFFFFFFF, {ease: FlxEase.linear});
+			FlxTween.color(frontCrowd, 1.3, 0xff474747, 0xFFFFFFFF, {ease: FlxEase.linear});
+			FlxTween.color(boyfriend, 1.3, 0xff474747, 0xFFFFFFFF, {ease: FlxEase.linear});
+			FlxTween.color(dad, 1.3, 0xff474747, 0xFFFFFFFF, {ease: FlxEase.linear});
+			FlxTween.color(gf, 1.3, 0xff474747, 0xFFFFFFFF, {ease: FlxEase.linear});
+			FlxTween.tween(star, {y: 2230}, 2.3, {ease: FlxEase.backInOut, onComplete: function(twn:FlxTween)
+			{
+				spin = false;
+				kuraCool = false;
+			}});
+		}
+	}
+
 	public function triggerEventNote(eventName:String, value1:String, value2:String) {
 		switch(eventName) {
 			case 'Bad Apple':
 				blackWhite();
 
 			case 'Cam Zoom':
-				var amount:Float = Std.parseFloat(value1);
+				if (ClientPrefs.camZooms) {
+					var amount:Float = Std.parseFloat(value1);
 
-				defaultCamZoom += amount;
+					defaultCamZoom += amount;
+				}
 
 			case 'Note Spin':
 				strumLineNotes.forEach(function(tospin:FlxSprite)
@@ -3186,6 +3320,53 @@ class PlayState extends MusicBeatState
 				{
 					spr.x += 1000;
 				});
+
+			case 'Nine Star Mode':
+				if(curStage == 'concert' && !kuraCool) {
+					kcmMode(Std.parseInt(value1));
+				}
+
+			case 'Camellia Zoom':
+				if(curStage == 'concert') {
+					var val1:Int = Std.parseInt(value1);
+					var val2:Null<Float> = Std.parseFloat(value2);
+
+					if(val2 == null)
+						val2 = 0.5;
+
+					if (ClientPrefs.camZooms) {
+						if (val1 == 1) {
+							zooming = true;
+							FlxTween.tween(frontCrowd, {y: 600, alpha: 1}, val2, {ease: FlxEase.sineIn});
+							FlxTween.tween(camGame, {zoom: 0.41}, val2, {ease: FlxEase.sineIn, onComplete: function(twn:FlxTween)
+							{
+								defaultCamZoom = camGame.zoom;
+							}});
+							FlxTween.tween(camHUD, {zoom: 0.8}, val2, {ease: FlxEase.sineIn, onComplete: function(twn:FlxTween)
+							{
+								FlxTween.tween(camHUD, {zoom: 0.8}, 0.00001, {ease: FlxEase.sineIn});
+							}});
+						}
+						if (val1 == 2) {
+							FlxTween.tween(frontCrowd, {y: 1000, alpha: 0}, val2, {ease: FlxEase.sineOut});
+							FlxTween.tween(camGame, {zoom: 0.59}, val2, {ease: FlxEase.sineOut, onComplete: function(twn:FlxTween)
+							{
+								defaultCamZoom = camGame.zoom;
+							}});
+							FlxTween.tween(camHUD, {zoom: 1}, val2, {ease: FlxEase.sineOut});
+							zooming = false;
+						}
+						if (val1 == 3) {
+							zooming = true;
+							FlxTween.tween(frontCrowd, {y: 600, alpha: 1}, val2, {ease: FlxEase.sineIn});
+							FlxTween.tween(camGame, {zoom: 0.47}, val2, {ease: FlxEase.sineIn});
+							FlxTween.tween(camHUD, {zoom: 0.9}, val2, {ease: FlxEase.sineIn, onComplete: function(twn:FlxTween)
+							{
+								FlxTween.tween(camHUD, {zoom: 0.9}, 0.00001, {ease: FlxEase.sineIn});
+							}});
+						}
+					}
+				}
 
 			case 'Dadbattle Spotlight':
 				var val:Null<Int> = Std.parseInt(value1);
@@ -4765,6 +4946,31 @@ class PlayState extends MusicBeatState
 			return;
 		}
 
+		if (curStep % 1 == 0 && spin) {
+			spin = false;
+			FlxTween.tween(star, {angle: 360}, 5, {ease: FlxEase.backInOut, onComplete: function(twn:FlxTween)
+			{
+				FlxTween.tween(star, {angle: 0}, 0.01, {ease: FlxEase.linear, onComplete: function(twn:FlxTween)
+				{
+					spin = true;
+				}});
+			}});
+		}
+		
+		switch(curStage)
+		{
+			case 'concert':
+				if (FlxG.keys.justPressed.LEFT && lightcycle == 'Player Lights') {
+					light.color = 0x00b1ff;
+				} else if (FlxG.keys.justPressed.UP && lightcycle == 'Player Lights') {
+					light.color = 0xff432c;
+				} else if (FlxG.keys.justPressed.DOWN && lightcycle == 'Player Lights') {
+					light.color = 0xff30fa;
+				} else if (FlxG.keys.justPressed.RIGHT && lightcycle == 'Player Lights') {
+					light.color = 0x00fd86;
+				}
+		}
+
 		lastStepHit = curStep;
 		setOnLuas('curStep', curStep);
 		callOnLuas('onStepHit', []);
@@ -4807,8 +5013,49 @@ class PlayState extends MusicBeatState
 		{
 			dad.dance();
 		}
+		// concert
 
-		if (zaBoom)
+		switch(curStage)
+		{
+			case 'concert':
+				if (curBeat % 2 == 0) {
+					speakerLeft.animation.play('bop');
+					speakerRight.animation.play('bop');
+					speakerLeft2.animation.play('bop');
+					speakerRight2.animation.play('bop');
+				} else if (curBeat % 3 == 0 && citycycle == 1 && lightcycle == 'Auto Lights') {
+					light.color = 0xff00b1ff; citycycle = 2;
+				} else if (curBeat % 3 == 0 && citycycle == 2 && lightcycle == 'Auto Lights') {
+					light.color =0xff432c; citycycle = 3;
+				} else if (curBeat % 3 == 0 && citycycle == 3 && lightcycle == 'Auto Lights') {
+					light.color = 0xff30fa; citycycle = 4;
+				} else if (curBeat % 3 == 0 && citycycle == 4 && lightcycle == 'Auto Lights') {
+					light.color =0x00fd86; citycycle = 5;
+				} else if (curBeat % 3 == 0 && citycycle == 5 && lightcycle == 'Auto Lights') {
+					light.color = 0xffa71f; citycycle = 1;
+				} 
+				else if (curBeat % 3 == 0 && citycycle == 1 && lightcycle == 'Slow Lights') {
+					FlxTween.color(light, 0.5, 0xffffa71f, 0xff00b1ff); citycycle = 2;
+				} else if (curBeat % 3 == 0 && citycycle == 2 && lightcycle == 'Slow Lights') {
+					FlxTween.color(light, 0.5, 0xff00b1ff, 0xffff432c); citycycle = 3;
+				} else if (curBeat % 3 == 0 && citycycle == 3 && lightcycle == 'Slow Lights') {
+					FlxTween.color(light, 0.5, 0xffff432c, 0xffff30fa); citycycle = 4;
+				} else if (curBeat % 3 == 0 && citycycle == 4 && lightcycle == 'Slow Lights') {
+					FlxTween.color(light, 0.5, 0xffff30fa, 0xff00fd86); citycycle = 5;
+				} else if (curBeat % 3 == 0 && citycycle == 5 && lightcycle == 'Slow Lights') {
+					FlxTween.color(light, 0.5, 0xff00fd86, 0xffffa71f); citycycle = 1;
+				}
+				if (curBeat % 2 == 0 && zooming) {
+					//Backcrowd bopping
+					FlxTween.tween(backCrowd.scale, {x: 1.85, y: 1.85}, 0.01, {ease: FlxEase.linear});
+					FlxTween.tween(backCrowd.scale, {x: 1.75, y: 1.75}, 0.5, {ease: FlxEase.quartOut});
+					//Frontcrowd bopping
+					FlxTween.tween(frontCrowd.scale, {x: 1.85, y: 1.85}, 0.01, {ease: FlxEase.linear});
+					FlxTween.tween(frontCrowd.scale, {x: 1.75, y: 1.75}, 0.5, {ease: FlxEase.quartOut});
+				}
+		}
+
+		if (zaBoom && ClientPrefs.camZooms)
 		{
 			FlxG.camera.zoom += 0.015 * boomCam;
 			camHUD.zoom += 0.03 * boomHud;

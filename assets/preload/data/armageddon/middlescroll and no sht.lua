8c0e45ef7043c3b352ece215ce50a6e9f76b6391
Joseph <josephjr1506@gmail.com>
2023-06-22 15:28:37 -0400

    Updated week json plus added GOG 112 COVERS!
    Check diffs.

---
+++ assets/preload/data/armageddon/middlescroll and no sht.lua
@@ -0,0 +1,13 @@
+--function onPause()
+	--health = getProperty('health')
+	--setProperty('health', health - 200);
+	--return Function_Stop;
+--end
+function onCreate()
+setProperty('healthBarBG.visible', false)
+setProperty('timeBarBG.visible', false)
+setProperty('timeBar.visible', false)
+setProperty('timeTxt.visible', false)
+setProperty('iconP1.visible', false)
+setProperty('iconP2.visible', false)
+end

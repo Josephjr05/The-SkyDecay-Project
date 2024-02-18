ef2eb12cbdb18ff41022fd74317b1c2f71067f05
Joseph <josephjr1506@gmail.com>
2024-02-17 22:34:24 -0500

    SunsetHigh stage updated by RexDX

--- assets/preload/stages/sunsetHigh.lua
+++ assets/preload/stages/sunsetHigh.lua
@@ -1,13 +1,6 @@
 function onCreate()
-	
+	makeLuaSprite('HighwayBG', 'stages/highway/sunsetHigh/highway', -600, -300)
+	setScrollFactor('HighwayBG', 0.9, 0.9)
 
-        -- sprites that only load if low Quality is turned off
-        if not lowQuality then
-	makeLuaSprite('highway', 'highway', -600, -300);
-	setLuaSpriteScrollFactor('highway', 0.9, 0.9);
-	end
-
-	addLuaSprite('highway', false);
-
-	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+	addLuaSprite('HighwayBG', false)
 end

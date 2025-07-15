525710141823e24777b5233efcc03938bd34bb0f
RowIsRowing <erykah0708@gmail.com>
2025-07-15 05:36:53 -0500

    Revert "Blackout event"
    This reverts commit aa162b4065f3531d1697ba125580ddd83b9e8ef4.

--- source/states/editors/ChartingState.hx
+++ source/states/editors/ChartingState.hx
@@ -111,7 +111,7 @@ class ChartingState extends MusicBeatState implements PsychUIEventHandler.PsychU
 		['Cinema Bars', "Toggles cinematic bars\n\nPlace event to use"],
 		['UI Fade', "Value 1: Alpha\nValue 2: Tween Time"],
 		['Flash Camera', "Value 1: Flash Timer"],
-		['BLACKOUT', "Value 1: To Fade In, do 1. To Fade out do 2.\nValue 2: Fade in and out speed (Measured in seconds)"],
+		// ['Black Camera Fades', "Place for black fade out."],
 		['Hide Health', "Value 1: 1 = appear, 0 = dissapear\nValue 2: Tween Time"],
 		['KM Toggle', "Toggles Kill Miss Mode\n\nValue 1: Max amount of misses"],
 		['RotScreenCam', "Value 1: Angle\nValue 2 Tween Time"],

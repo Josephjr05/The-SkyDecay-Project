b0c3b3f810cbc96c2469a8fa55bff19c4fe46f5c
Joseph <josephjr1506@gmail.com>
2025-11-29 00:39:44 -0500

    Deleted flixel stuff for latest libs

--- source/flixel/util/IDestroyable.hx
+++
@@ -1,9 +0,0 @@
-package flixel.util;
-
-/**
- * A simple interface for objects that can be destroyed and pooled.
- * This replaces the internal FlxDestroyable class for external use from earlier flixel versions.
- */
-interface IDestroyable {
-    public function destroy():Void;
-}

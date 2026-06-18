eee2dd715235eddcfe10176741af57486ec9fafe
Joseph <josephjr1506@gmail.com>
2026-06-18 19:44:03 -0400

    Event called 3 ima die
    Rowan stop getting errors

--- source/backend/BaseStage.hx
+++ source/backend/BaseStage.hx
@@ -88,7 +88,7 @@ class BaseStage extends FlxBasic
 	public function openSubState(SubState:FlxSubState) {}
 
 	// Events
-	public function eventCalled(eventName:String, value1:String, value2:String, flValue1:Null<Float>, flValue2:Null<Float>, strumTime:Float) {}
+	public function eventCalled(eventName:String, value1:String, value2:String, value3:String, flValue1:Null<Float>, flValue2:Null<Float>, strumTime:Float) {}
 	public function eventPushed(event:EventNote) {}
 	public function eventPushedUnique(event:EventNote) {}
 
